c692ec62d2fb4a821db000de514517a58b71b507
Andrew Nieves <tun29995@temple.edu>
2026-03-21 18:46:26 -0400

    Initialized app structure (backend)

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "app" # if we end up changing the name of the app folder, change it here too
 ]
 
 MIDDLEWARE = [
