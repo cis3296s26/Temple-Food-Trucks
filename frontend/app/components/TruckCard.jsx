e7b77373cc8d4baa4e3dc5e7bb894c02f1e93bbb
Felix Haardjana <fhardjana@gmail.com>
2026-03-31 14:58:01 -0400

    added a image placeholder

--- frontend/app/components/TruckCard.jsx
+++ frontend/app/components/TruckCard.jsx
@@ -3,14 +3,20 @@ export default function TruckCard({truckName}){
     
     return (
         <>
-        <div className="flex justify-center bg-blue-500 mb-4 p-4 w-5/12 rounded-4xl"> 
-            <div className="">
+        <div className="flex justify-center"> {/* Keep this for now so that the box is centered */}
+            <div className="bg-blue-500 mb-4 w-5/12 rounded-4xl border-4 m-5 "> {/* This is to stylize the content inside the box */}
+                <div className="bg-amber-800 h-9/12 rounded-t-4xl"> 
+                    {/*this is an image placeholder*/}
+                </div>
+                
+                <div className="">
 
-                <h1 className="text-center">{truckName}</h1> {/*This is the parameter for the food truck*/}
-                <hr/>
-                <p>This is the description for the food truck. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt odio quis cum commodi ullam harum velit atque culpa quod molestias nihil amet quam sapiente, exercitationem sit optio saepe aliquam cupiditate.</p>
-            </div>  
-        </div>      
+                    <h1 className="text-center">{truckName}</h1> {/*This is the parameter for the food truck*/}
+                    <hr/>
+                    <p>This is the description for the food truck. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt odio quis cum commodi ullam harum velit atque culpa quod molestias nihil amet quam sapiente, exercitationem sit optio saepe aliquam cupiditate.</p>
+                </div>  
+            </div>     
+        </div> 
         </>
     )
 }
