f4e8e57cd7d86838af2d8ae43b01f2e7fdff12d6
Felix Haardjana <fhardjana@gmail.com>
2026-04-07 14:50:35 -0400

    added search bar and creating signup page

---
+++ frontend/app/signup/page.js
@@ -0,0 +1,9 @@
+import { PageMain } from "../components/PageMain";
+
+export default function SignUp(){
+    return(
+        <PageMain>
+            <h1>This is the signup page</h1>
+        </PageMain>
+    )
+}
