ed5326072d292b534cb06ea1ececf811911c8d15
Andrew Nieves <tun29995@temple.edu>
2026-04-24 00:02:13 -0400

    updated settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -181,6 +181,7 @@ SIMPLE_JWT = {
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.SessionAuthentication', 
         'rest_framework_simplejwt.authentication.JWTAuthentication',        
     ),
 }
